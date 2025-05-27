9ee80f365cceb5cbc8dcd838e109f555f2969ac2
arBishal <arbishal.319@gmail.com>
2025-05-27 16:45:45 +0600

    chore: extract hero section as seperate component

---
+++ components/landing-page/hero.jsx
@@ -0,0 +1,21 @@
+import TypewriterEffect from "@/components/typewriter-effect";
+
+export default function Hero() {
+  return (
+    <div className="max-w-5xl p-6 gap-2 flex flex-col items-center justify-center text-6xl font-extrabold">
+      <h2>Ahoy! I'm Bishal.</h2>
+      <h2>
+        A{" "}
+        <span className="inline-block">
+          <TypewriterEffect
+            texts={[
+              "Software Engineer.",
+              "Creative Writer.",
+              "Graphic Designer.",
+            ]}
+          />
+        </span>
+      </h2>
+    </div>
+  );
+}
