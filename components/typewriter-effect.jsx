1b9a4044fc07cba96738ab4e538759c2972b458d
arBishal <arbishal.319@gmail.com>
2025-05-27 16:41:01 +0600

    feat: add typewriter effect

---
+++ components/typewriter-effect.jsx
@@ -0,0 +1,26 @@
+"use client";
+import Typewriter from "typewriter-effect";
+
+export default function TypewriterText({ texts = [] }) {
+  const defaultTexts = ["Software Engineer"];
+
+  const styledTexts = (texts.length > 0 ? texts : defaultTexts).map(
+    (text) =>
+      `<span class='text-transparent bg-clip-text bg-gradient-to-r from-emerald-400 to-teal-600'>${text}</span>`
+  );
+
+  return (
+    <div className="text-6xl font-extrabold">
+      <Typewriter
+        options={{
+          strings: styledTexts,
+          autoStart: true,
+          loop: true,
+          delay: 100,
+          deleteSpeed: 50,
+          html: true,
+        }}
+      />
+    </div>
+  );
+}
