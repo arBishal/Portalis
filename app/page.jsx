98f8760296f60ad0635f31f26b163205729f5b0d
arBishal <arbishal.319@gmail.com>
2025-06-11 02:47:27 +0600

    feat: add atelier section

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/landing-page/hero";
+import Atelier from "@/components/landing-page/atelier";
 
 export default function Home() {
   return (
     <>
       <Hero/>
+      <Atelier />
     </>
   );
 }
