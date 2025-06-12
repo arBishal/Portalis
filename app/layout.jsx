0b034df32157a59efe50e4691f0e2df6c5f61080
arBishal <arbishal.319@gmail.com>
2025-06-12 19:05:20 +0600

    feat: add footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 import Navbar from "@/components/layout/navbar";
+import Footer from "@/components/layout/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         <main className="flex flex-col items-center justify-center min-h-screen font-mono">
           {children}
         </main>
+        <Footer />
       </body>
     </html>
   );
---
+++ components/layout/footer.jsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <div className="px-6 py-4 w-full border-t border-border-transparent font-mono text-sm sm:text-base">
+      <div className="max-w-5xl m-auto flex gap-2 flex-col sm:flex-row justify-between items-center">
+        <span className="text-left">Portalis: A Portfolio Template </span>
+        <span className="text-right"><a href="https://github.com/arBishal/portalis" className="hover:underline">Find the Source Code Here</a>.</span>
+      </div>
+    </div>
+  );
+}
