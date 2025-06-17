fde34da985946151ec2a877dd2f3b8ef691cf5bd
arBishal <arbishal.319@gmail.com>
2025-06-17 16:54:03 +0600

    feat: create and use theme context

--- components/layout/navbar.jsx
+++ components/layout/navbar.jsx
@@ -1,54 +1,23 @@
 "use client";
 
 import Link from "next/link";
-import { useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
-import {
-  SunIcon,
-  MoonIcon,
-} from "@heroicons/react/24/outline";
-
-import { useScrollNavbar } from "@/hooks/useScrollNavbar";
-import FlipMotionEffect from "@/components/effects/flip-motion-effect";
-import { NAV_LINKS } from "@/constants/nav-links.js";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/outline";
+import { useTheme } from "@/context/theme-context";
+import { NAV_LINKS } from "@/constants/nav-links";
 
 export default function Navbar() {
-  const pathname = usePathname();
-  const [isDark, setIsDark] = useState(true);
-
-  useEffect(() => {
-    const root = document.documentElement;
-    root.classList.toggle("dark", isDark);
-    root.classList.toggle("light", !isDark);
-  }, [isDark]);
-
-  const toggleTheme = () => {
-    setIsDark(!isDark);
-  };
-
-  // const { visible, mounted } = useScrollNavbar(10);
+  const { isDark, toggleTheme } = useTheme();
 
   return (
-    <nav
-      className={`fixed font-mono flex items-center justify-center sm:justify-between gap-24 px-6 py-4 border border-border-transparent backdrop-blur-2xl rounded-full w-min max-w-5xl top-4 left-1/2 transform -translate-x-1/2 transition-all duration-300 ease-in-out z-10`}
-    >
-      {/* logo */}
+    <nav className="fixed font-mono flex items-center justify-center sm:justify-between gap-24 px-6 py-4 border border-border-transparent backdrop-blur-2xl rounded-full w-min max-w-5xl top-4 left-1/2 transform -translate-x-1/2 transition-all duration-300 ease-in-out z-10">
       <Link href="/#hero" className="font-black hidden sm:block">PO.RTALIS</Link>
 
-      {/* links */}
       <div className="flex gap-4 sm:gap-6 text-sm sm:text-lg items-center justify-center">
         {NAV_LINKS.map(({ href, label, icon: Icon }, index) => (
-        <Link
-          key={index}
-          href={href}
-          aria-label={label}
-          title={label}
-        >
-          <Icon className="w-5 h-5 sm:w-6 sm:h-6 hover:text-teal-500 transition-transform duration-300 active:-rotate-45" />
-        </Link>
-      ))}
-
-        {/* theme toggle */}
+          <Link key={index} href={href} aria-label={label} title={label}>
+            <Icon className="w-5 h-5 sm:w-6 sm:h-6 hover:text-teal-500 transition-transform duration-300 active:-rotate-45" />
+          </Link>
+        ))}
         <button
           onClick={toggleTheme}
           className="transition-transform duration-500 active:-rotate-90 cursor-pointer"
